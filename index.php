ba3068b5b040c29063d7da668e99502c8b120141
parishenaina <parisheymriganayana@gmail.com>
2016-09-19 00:24:27 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>MRIGANAYANA (14968)HW 1</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <h1> Multi-Device Web-site </h1>
+        <p> From smartphones to tablets to everything in between, a wider variety of 
+computing devices than ever before are being used to get online. 
+These devices have different screen sizes and resolutions, input methods, 
+and even different modes of use. Most organizations have responded to 
+this new reality by creating separate experiences for new devices types: 
+a separate mobile site, a separate tablet site, and so on. But todayâ€™s 
+devices are blurring even these lines. </p>
+    <img src="UqgNEze_.jpg" width="200" height="200" alt="UqgNEze_"/>
+<br>
+    <a href="http://npu.edu/">NPU Website</a><br>
+    <a href="table.html">Table Page</a><br>
+    <a href="index.html">Top</a>
+    </body>
+</html>
---
+++ table.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>Table Page</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <table border="1">
+           
+            <tbody>
+                 <tr>
+   <th colspan=4 rowspan=1>HTML5</th>
+   
+   </tr>
+    <tr>
+   <th colspan=4 rowspan=1>Table</th>
+   
+   </tr>
+    <tr>
+   <th colspan=2 rowspan=1>Name</th>
+   <th colspan=2 rowspan=1>Address</th>
+   
+   </tr>
+    <tr>
+   <th colspan=1 rowspan=1>FirstName</th>
+   <th colspan=1 rowspan=1>LastName</th>
+   <th colspan=1 rowspan=1>State</th>
+   <th colspan=1 rowspan=1>City</th>
+   </tr>
+    <tr>
+   <th colspan=1 rowspan=1>jon</th>
+   <th colspan=1 rowspan=1>doe</th>
+   <th colspan=1 rowspan=1>CA</th>
+   <th colspan=1 rowspan=1>Fremont</th>
+   </tr>
+    <tr>
+   <th colspan=1 rowspan=1>Eve</th>
+   <th colspan=1 rowspan=1>Jackson</th>
+   <th colspan=1 rowspan=1>CA</th>
+   <th colspan=1 rowspan=1>SanJose</th>
+   </tr>
+    <tr>
+   <th colspan=1 rowspan=1>Adam</th>
+   <th colspan=1 rowspan=1>Smith</th>
+   <th colspan=1 rowspan=1>CA</th>
+   <th colspan=1 rowspan=1>Milpitas</th>
+   </tr>
+            </tbody>
+        </table>
+
+    </body>
+</html>
